a6960d98017996af1ebd5668ac40b95ed1c209a0
Adam Bird <archez39@me.com>
2024-12-31 19:12:32 -0500

    Convert BowReticle to ShipInit

--- mm/2s2h/Enhancements/Graphics/BowReticle.cpp
+++ mm/2s2h/Enhancements/Graphics/BowReticle.cpp
@@ -1,12 +1,16 @@
 #include "libultraship/libultraship.h"
 #include "2s2h/GameInteractor/GameInteractor.h"
 #include "2s2h/Enhancements/FrameInterpolation/FrameInterpolation.h"
+#include "2s2h/ShipInit.hpp"
 
 extern "C" {
 #include "functions.h"
 #include "variables.h"
 #include "objects/gameplay_keep/gameplay_keep.h"
 }
+
+#define CVAR_NAME "gEnhancements.Graphics.BowReticle"
+#define CVAR CVarGetInteger(CVAR_NAME, 0)
 
 void DrawBowReticle(PlayState* play, Player* player, f32 bowDistance) {
     static Vec3f D_801C094C = { -500.0f, -100.0f, 0.0f };
@@ -50,22 +54,22 @@ void DrawBowReticle(PlayState* play, Player* player, f32 bowDistance) {
 }
 
 void RegisterBowReticle() {
-    GameInteractor::Instance->RegisterGameHookForID<GameInteractor::OnPlayerPostLimbDraw>(
-        PLAYER_LIMB_RIGHT_HAND, [](Player* player, s32 limbIndex) {
-            if (player->actor.scale.y >= 0.0f &&
-                ((player->heldItemAction == PLAYER_IA_BOW_FIRE) || (player->heldItemAction == PLAYER_IA_BOW_ICE) ||
-                 (player->heldItemAction == PLAYER_IA_BOW_LIGHT) || (player->heldItemAction == PLAYER_IA_BOW)) &&
-                CVarGetInteger("gEnhancements.Graphics.BowReticle", 0)) {
+    COND_ID_HOOK(OnPlayerPostLimbDraw, PLAYER_LIMB_RIGHT_HAND, CVAR, [](Player* player, s32 limbIndex) {
+        if (player->actor.scale.y >= 0.0f &&
+            ((player->heldItemAction == PLAYER_IA_BOW_FIRE) || (player->heldItemAction == PLAYER_IA_BOW_ICE) ||
+             (player->heldItemAction == PLAYER_IA_BOW_LIGHT) || (player->heldItemAction == PLAYER_IA_BOW))) {
 
-                if (func_800B7128(player) != 0) {
-                    // Rotation from link's right hand that aligns with arrow projection
-                    Matrix_RotateZYX(0, -0x3B33, -0x4423, MTXMODE_APPLY);
-                    Matrix_Translate(575.0f, 345.0f, 0.0f, MTXMODE_APPLY);
+            if (func_800B7128(player) != 0) {
+                // Rotation from link's right hand that aligns with arrow projection
+                Matrix_RotateZYX(0, -0x3B33, -0x4423, MTXMODE_APPLY);
+                Matrix_Translate(575.0f, 345.0f, 0.0f, MTXMODE_APPLY);
 
-                    // 341000 as a value is selected roughly as a ratio of the hookshot value to maximum hookshot
-                    // distance from player, multiplied by maximum arrow distance from player -- (77600 / 770) * 3385
-                    DrawBowReticle(gPlayState, player, 341000.0f);
-                }
+                // 341000 as a value is selected roughly as a ratio of the hookshot value to maximum hookshot
+                // distance from player, multiplied by maximum arrow distance from player -- (77600 / 770) * 3385
+                DrawBowReticle(gPlayState, player, 341000.0f);
             }
-        });
+        }
+    });
 }
+
+static RegisterShipInitFunc initFunc(RegisterBowReticle, { CVAR_NAME });
